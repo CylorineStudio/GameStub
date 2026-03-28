c36862f6873d12751166bf7bdca3c8b54f6faa30
AnemoFlower <yizhimcqiu@icloud.com>
2026-03-28 16:13:46 +0800

    update

--- Sources/Runner/main.swift
+++ Sources/Runner/main.swift
@@ -9,14 +9,12 @@ import Foundation
 import AppKit
 
 func launch() {
-//    var pid: pid_t = 0
     let workingDirectory: String = ProcessInfo.processInfo.arguments[1]
     chdir(workingDirectory)
     let arguments: [String] = Array(ProcessInfo.processInfo.arguments.dropFirst(2))
     let executablePath: String = arguments[0]
     let argv: [UnsafeMutablePointer<CChar>?] = arguments.map { strdup($0) } + [nil]
     argv.withUnsafeBufferPointer { buffer in
-//        posix_spawn(&pid, executablePath, nil, nil, buffer.baseAddress, nil)
         execv(executablePath, buffer.baseAddress)
         perror("execv")
     }
@@ -25,8 +23,6 @@ func launch() {
 
 class ApplicationDelegate: NSObject, NSApplicationDelegate {
     func applicationWillFinishLaunching(_ notification: Notification) {
-//        CFRunLoopRunInMode(.defaultMode, 1, true)
-//        CFRunLoopRunInMode(.defaultMode, 1, true)
         launch()
     }
 }
